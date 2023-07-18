e7e04a69f7ec2863b74a61002ec957b3fb3ea7f4
Krzysztof Stelmach <kcezarystelmach@gmail.com>
2023-07-18 13:50:26 +0200

    mumbai config

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,6 +1,6 @@
-'use client';
+'use client'
 
-import * as React from 'react';
+import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { ThemeProviderProps } from 'next-themes/dist/types'
 import { TooltipProvider } from '@/components/ui/tooltip'
@@ -8,20 +8,16 @@ import { TooltipProvider } from '@/components/ui/tooltip'
 import {
   RainbowKitProvider,
   getDefaultWallets,
-  connectorsForWallets,
-} from '@rainbow-me/rainbowkit';
+  connectorsForWallets
+} from '@rainbow-me/rainbowkit'
 import {
   argentWallet,
   trustWallet,
-  ledgerWallet,
-} from '@rainbow-me/rainbowkit/wallets';
-import { 
-  configureChains, 
-  createConfig, 
-  WagmiConfig 
-} from 'wagmi';
+  ledgerWallet
+} from '@rainbow-me/rainbowkit/wallets'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
 
-import { publicProvider } from 'wagmi/providers/public';
+import { publicProvider } from 'wagmi/providers/public'
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
@@ -29,31 +25,42 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
     // polygon,
     // ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli, mumbai] : []),
     {
+      id: 80001,
       name: 'Mumbai',
-      chainId: 80001,
-      rpcUrls: ['https://rpc-mumbai.maticvigil.com'],
+      network: 'Mumbai',
+      rpcUrls: {
+        default: {
+          http: ['https://rpc-mumbai.maticvigil.com'],
+          webSocket: ['wss://rpc-mumbai.maticvigil.com/ws/v1/']
+        },
+        public: {
+          http: ['https://rpc-mumbai.maticvigil.com'],
+          webSocket: ['wss://rpc-mumbai.maticvigil.com/ws/v1/']
+        }
+      },
       blockExplorerUrls: ['https://explorer-mumbai.maticvigil.com'],
       nativeCurrency: {
         name: 'Matic',
         symbol: 'MATIC',
-        decimals: 18,
+        decimals: 18
       },
+      testnet: true
     }
   ],
   [publicProvider()]
-);
+)
 
-const projectId = process.env.NEXT_PUBLIC_WC2_PROJECT_ID || '';
+const projectId = process.env.NEXT_PUBLIC_WC2_PROJECT_ID || ''
 
 const { wallets } = getDefaultWallets({
   appName: 'RainbowKit demo',
   projectId,
-  chains,
-});
+  chains
+})
 
 const demoAppInfo = {
-  appName: 'Rainbowkit Demo',
-};
+  appName: 'Rainbowkit Demo'
+}
 
 const connectors = connectorsForWallets([
   ...wallets,
@@ -62,21 +69,21 @@ const connectors = connectorsForWallets([
     wallets: [
       argentWallet({ projectId, chains }),
       trustWallet({ projectId, chains }),
-      ledgerWallet({ projectId, chains }),
-    ],
-  },
-]);
+      ledgerWallet({ projectId, chains })
+    ]
+  }
+])
 
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors,
   publicClient,
-  webSocketPublicClient,
-});
+  webSocketPublicClient
+})
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
-  const [mounted, setMounted] = React.useState(false);
-  React.useEffect(() => setMounted(true), []);
+  const [mounted, setMounted] = React.useState(false)
+  React.useEffect(() => setMounted(true), [])
 
   return (
     <NextThemesProvider {...props}>
@@ -88,5 +95,5 @@ export function Providers({ children, ...props }: ThemeProviderProps) {
         </WagmiConfig>
       </TooltipProvider>
     </NextThemesProvider>
-  );
+  )
 }
