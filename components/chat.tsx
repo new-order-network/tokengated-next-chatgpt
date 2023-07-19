ba4f8c964c4977edcd65a8d1e312f1ce60f581ba
Krzysztof Stelmach <kcezarystelmach@gmail.com>
2023-07-19 08:35:03 +0200

    chat wip implementation

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import { useChat, type Message } from 'ai/react'
-
+import { useState } from 'react'
 import { cn } from '@/lib/utils'
 import { ChatList } from '@/components/chat-list'
 import { ChatPanel } from '@/components/chat-panel'
 import { EmptyScreen } from '@/components/empty-screen'
 import { ChatScrollAnchor } from '@/components/chat-scroll-anchor'
-import { useLocalStorage } from '@/lib/hooks/use-local-storage'
+
 import {
   Dialog,
   DialogContent,
@@ -16,38 +16,77 @@ import {
   DialogHeader,
   DialogTitle
 } from '@/components/ui/dialog'
-import { useState } from 'react'
 import { Button } from './ui/button'
 import { Input } from './ui/input'
 import { toast } from 'react-hot-toast'
-
 const IS_PREVIEW = process.env.VERCEL_ENV === 'preview'
+
 export interface ChatProps extends React.ComponentProps<'div'> {
   initialMessages?: Message[]
   id?: string
 }
 
-export function Chat({ id, initialMessages, className }: ChatProps) {
-  const [previewToken, setPreviewToken] = useLocalStorage<string | null>(
-    'ai-token',
-    null
+export function Chat({
+  id,
+  initialMessages,
+  className
+}: ChatProps): JSX.Element {
+  useChat()
+  const [previewToken, setPreviewToken] = useState<string | null>('ai-token')
+  const [previewTokenDialog, setPreviewTokenDialog] =
+    useState<boolean>(IS_PREVIEW)
+  const [previewTokenInput, setPreviewTokenInput] = useState<string>(
+    previewToken ?? ''
   )
-  const [previewTokenDialog, setPreviewTokenDialog] = useState(IS_PREVIEW)
-  const [previewTokenInput, setPreviewTokenInput] = useState(previewToken ?? '')
-  const { messages, append, reload, stop, isLoading, input, setInput } =
-    useChat({
-      initialMessages,
-      id,
-      body: {
-        id,
-        previewToken
-      },
-      onResponse(response) {
-        if (response.status === 401) {
-          toast.error(response.statusText)
-        }
+  const [messages, setMessages] = useState<Message[]>(initialMessages || [])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [input, setInput] = useState<string>('')
+
+  const fetchLastUserMessage = async (): Promise<void> => {
+    try {
+      setIsLoading(true)
+
+      const payload = {
+        messages: [
+          {
+            role: 'user',
+            content: input
+          }
+        ]
       }
-    })
+
+      const response = await fetch(
+        'http://157.245.65.240:8000/get_last_user_message/',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(payload)
+        }
+      )
+
+      if (!response.ok) {
+        throw new Error('An error occurred')
+      }
+
+      const data = await response.json()
+
+      const newMessage: Message = {
+        id: data.id,
+        role: 'assistant',
+        content: data
+      }
+
+      setMessages(prevMessages => [...prevMessages, newMessage])
+      setIsLoading(false)
+      setInput('')
+    } catch (error) {
+      setIsLoading(false)
+      toast.error('An error occurred. Please try again.')
+    }
+  }
+
   return (
     <>
       <div className={cn('pb-[200px] pt-4 md:pt-10', className)}>
@@ -63,9 +102,9 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
       <ChatPanel
         id={id}
         isLoading={isLoading}
-        stop={stop}
-        append={append}
-        reload={reload}
+        stop={fetchLastUserMessage}
+        append={fetchLastUserMessage as any}
+        reload={fetchLastUserMessage as any}
         messages={messages}
         input={input}
         setInput={setInput}
